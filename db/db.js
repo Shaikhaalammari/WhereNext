03f1ea46020a848ecdbbdeda5cdbdd78024db2e6
alia-alabduljalil <aliaanmar@gmail.com>
2020-08-27 17:17:08 +0300

    a few changes

--- db/db.js
+++ db/db.js
@@ -1,16 +1,11 @@
 const { Sequelize } = require("sequelize");
 
 const db = new Sequelize({
-
   username: "postgres",
   password: "pinksummer",
   database: "wherenext",
   dialect: "postgres",
   host: "localhost",
-
-  dialect: "sqlite",
-  storage: "my_db.db",
-
   logging: false,
 });
 
